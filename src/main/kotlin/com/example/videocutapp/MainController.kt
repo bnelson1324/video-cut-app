d593a93e385d31646845f34b54024e1ff80bbc71
Benjamin Nelson <bnelson1324@users.noreply.github.com>
2023-12-31 10:10:31 -0800

    implemented reading files from open directory and displaying the media

--- src/main/kotlin/com/example/videocutapp/MainController.kt
+++ src/main/kotlin/com/example/videocutapp/MainController.kt
@@ -4,11 +4,22 @@ import javafx.event.ActionEvent
 import javafx.fxml.FXML
 import javafx.scene.Node
 import javafx.scene.control.Label
+import javafx.scene.media.Media
+import javafx.scene.media.MediaPlayer
+import javafx.scene.media.MediaView
 import javafx.stage.DirectoryChooser
 import java.io.File
 
 class MainController {
-    private lateinit var openDirectory: File
+    private var openDirectory: File? = null
+
+    private var videoList: Array<File> = arrayOf()
+
+    private var mediaPlayerIndex = 0
+
+
+    @FXML
+    private lateinit var mediaView: MediaView
 
     @FXML
     private lateinit var openDirectoryLabel: Label
@@ -21,10 +32,20 @@ class MainController {
 
     @FXML
     fun onChooseDirectoryBtnClick(ae: ActionEvent) {
+        // prompt user to choose a directory
         val directoryChooser = DirectoryChooser()
         directoryChooser.title = "Choose folder with videos"
-        openDirectory = directoryChooser.showDialog((ae.source as Node).scene.window) ?: return
+        openDirectory = directoryChooser.showDialog((ae.source as Node).scene.window)
+        if (openDirectory == null)
+            return
+
+        // update video list and media player
         openDirectoryLabel.text = "Source Directory: $openDirectory"
-        // TODO: update video list
+        videoList = openDirectory!!.listFiles()!! // TODO: filter files to be video files only
+        updateMediaPlayer()
+    }
+
+    private fun updateMediaPlayer() {
+        mediaView.mediaPlayer = MediaPlayer(Media(videoList[mediaPlayerIndex].toURI().toString()))
     }
 }
